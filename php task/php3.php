2f774d6fd3419b823da3b2f67045c85b562f9c3a
poromabasak248 <poromabasak248@gmail.com>
2026-04-08 21:49:19 +0600

    PHP First task

---
+++ php task/php3.php
@@ -0,0 +1,14 @@
+<?php
+$number = 10;
+
+echo "Given number = {$number} <br>";
+
+if($number % 2 == 0){
+    echo "The given number is EVEN";
+}
+else{
+    echo "The given number is ODD";
+}
+
+
+?>
